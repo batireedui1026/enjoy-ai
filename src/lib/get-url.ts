f40c960d842523c2ec244207e21fd05e4405c83b
batireedui <batireeduiotgonsukh@gmail.com>
2025-09-13 15:45:08 +0800

    fix backend

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -1,6 +1,11 @@
 export const getUrl = () => {
   //   return window.location.origin + "/api/v1";
+
+  // return (
+  //   window.location.protocol + "//" + window.location.hostname + ":8001/api/v1"
+  // );
   return (
-    window.location.protocol + "//" + window.location.hostname + ":8001/api/v1"
+    "Enjoyai-backend-env.eba-f5a4zymk.ap-southeast-1.elasticbeanstalk.com " +
+    "/api/v1"
   );
 };
