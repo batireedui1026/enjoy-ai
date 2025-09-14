c1db660a7ae2e6350a0bc0e2d9ee123531ec2205
batireedui <batireeduiotgonsukh@gmail.com>
2025-09-14 14:52:18 +0800

    url

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -4,8 +4,5 @@ export const getUrl = () => {
   // return (
   //   window.location.protocol + "//" + window.location.hostname + ":8001/api/v1"
   // );
-  return (
-    "http://enjoyai-backend-env.eba-f5a4zymk.ap-southeast-1.elasticbeanstalk.com/ " +
-    "/api/v1"
-  );
+  return "https://backend.steam.mn" + "/api/v1";
 };
