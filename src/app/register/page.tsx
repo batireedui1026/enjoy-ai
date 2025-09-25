9ba0b8358b60d9dc9adcc4788b4840966a7db5e4
batireedui <batireeduiotgonsukh@gmail.com>
2025-09-25 12:47:04 +0800

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,381 +1,3 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import { getUrl } from "../../lib/get-url";
-// import Image from "next/image";
-// import { ClassData, Province } from "../../lib/type";
-// import RegisterConfirm from "../../components/registerConfirm";
-
-// export default function RegistrationForm() {
-//   const [next, setNext] = useState(false);
-//   const [classData, setClassData] = useState<ClassData[]>([]);
-//   const [provinceData, setProvinceData] = useState<Province[]>([]);
-//   const [form, setForm] = useState({
-//     province: "",
-//     trainingType: "",
-//     FirstName: "",
-//     LastName: "",
-//     age: "",
-//     gender: "",
-//     employmentStatus: "",
-//     contactPhone: "",
-//     email: "",
-//     paymentStatus: "unpaid",
-//   });
-//   const [formErrorState, setFormErrorState] = useState({
-//     province: false,
-//     trainingType: false,
-//     FirstName: false,
-//     LastName: false,
-//     age: false,
-//     gender: false,
-//     employmentStatus: false,
-//     contactPhone: false,
-//     email: false,
-//     paymentStatus: false,
-//     agreedToTerms: false,
-//   });
-
-//   const getProvince = async () => {
-//     try {
-//       const response = await axios.get(`${getUrl()}/province`);
-//       if (response.status === 200) {
-//         setProvinceData(response.data?.location || []);
-//       }
-//     } catch (error) {
-//       console.error("Алдаа:", error);
-//     }
-//   };
-
-//   const getClass = async () => {
-//     try {
-//       const response = await axios.get(`${getUrl()}/lesson`);
-//       console.log("class.....", response.data?.subjects);
-//       if (response.status === 200) {
-//         setClassData(response.data?.subjects || []);
-//       }
-//     } catch (error) {
-//       console.error("Алдаа:", error);
-//     }
-//   };
-
-//   const isValidEmail = (email: string) =>
-//     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
-//   const handleCreateInvoice = () => {
-//     const errors = { ...formErrorState };
-
-//     if (form.province === "") errors.province = true;
-//     if (form.trainingType === "") errors.trainingType = true;
-//     if (form.FirstName === "") errors.FirstName = true;
-//     if (form.LastName === "") errors.LastName = true;
-//     if (form.age === "") errors.age = true;
-//     if (form.contactPhone === "" || form.contactPhone.length !== 8)
-//       errors.contactPhone = true;
-//     if (form.email === "" || !isValidEmail(form.email)) errors.email = true;
-//     if (form.gender === "") errors.gender = true;
-//     if (form.employmentStatus === "") errors.employmentStatus = true;
-
-//     setFormErrorState(errors);
-//     const allFalse = Object.values(errors).every((value) => value === false);
-//     if (allFalse) setNext(true);
-//   };
-
-//   useEffect(() => {
-//     getProvince();
-//     getClass();
-//   }, []);
-
-//   return (
-//     <>
-//       {next ? (
-//         <RegisterConfirm
-//           className="flex flex-col items-center justify-center w-full h-full"
-//           classData={classData}
-//           form={form}
-//           provinceData={provinceData}
-//         />
-//       ) : (
-//         <div className="max-w-3xl mx-auto">
-//           <div className="bg-gradient-to-r from-blue-600 to-green-500 text-white rounded-t-xl py-4 px-6 shadow-md">
-//             <h1 className="text-2xl font-bold text-center">EnjoyAi бүртгэл</h1>
-//           </div>
-
-//           <div className="bg-gray-100 2xl:p-4 xl:p-4 lg:p-4 md:p-2 max-md:p-2 max-sm:p-2 sm:p-2 m-0 rounded-b-lg">
-//             <div className="my-2">
-//               <Image
-//                 className="bg-cover"
-//                 src="/image/logos.png"
-//                 alt="unitelhub"
-//                 width={750}
-//                 height={600}
-//               />
-//             </div>
-//             <div className="bg-white p-6 xl:p-6 lg:p-6 md:p-2 max-md:p-2 max-sm:p-2 sm:p-2 rounded-lg shadow-sm">
-//               <div className="mb-6">
-//                 <label className="font-bold block mb-2">
-//                   1. Cургалтын төрөл
-//                 </label>
-//                 <select
-//                   value={form.trainingType}
-//                   onChange={(e) => {
-//                     setForm((prevState) => ({
-//                       ...prevState,
-//                       trainingType: e.target.value,
-//                     }));
-//                     setFormErrorState((prevState) => ({
-//                       ...prevState,
-//                       trainingType: false,
-//                     }));
-//                   }}
-//                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                 >
-//                   <option value="">Сургалтын төрөл сонгоно уу</option>
-//                   {classData.map(({ _id, name, ageLimitMin, ageLimitMax }) => (
-//                     <option key={_id} value={_id}>
-//                       {name} {ageLimitMin}-{ageLimitMax} нас
-//                     </option>
-//                   ))}
-//                 </select>
-//                 {formErrorState.trainingType && (
-//                   <p className="text-red-600 text-sm">
-//                     Сургалтын төрөл сонгоно уу
-//                   </p>
-//                 )}
-//               </div>
-//               <div className="mb-6">
-//                 <label className="font-bold block mb-2">2. Байршил</label>
-//                 <select
-//                   value={form.province}
-//                   onChange={(e) => {
-//                     setForm((prevState) => ({
-//                       ...prevState,
-//                       province: e.target.value,
-//                     }));
-//                     setFormErrorState((prevState) => ({
-//                       ...prevState,
-//                       province: false,
-//                     }));
-//                   }}
-//                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                 >
-//                   <option value="">Aймаг сонгоно уу</option>
-//                   {provinceData.map(({ _id, name }) => (
-//                     <option key={_id} value={_id}>
-//                       {name}
-//                     </option>
-//                   ))}
-//                 </select>
-//                 {formErrorState.province && (
-//                   <p className="text-red-600 text-sm">Аймаг сонгоно уу</p>
-//                 )}
-//               </div>
-//               <div>
-//                 <label className="font-bold block mb-2">
-//                   3. Сургуулийн нэр
-//                 </label>
-//                 <input
-//                   value={form.FirstName}
-//                   onChange={(e) => {
-//                     setForm((prevState) => ({
-//                       ...prevState,
-//                       FirstName: e.target.value,
-//                     }));
-//                     setFormErrorState((prevState) => ({
-//                       ...prevState,
-//                       FirstName: false,
-//                     }));
-//                   }}
-//                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                   placeholder="Нэр"
-//                 />
-//                 {formErrorState.FirstName && (
-//                   <p className="text-red-600 text-sm">Нэрээ бичнэ үү</p>
-//                 )}
-//               </div>
-//               <div>
-//                 <label className="font-bold block mb-2 mt-2">
-//                   4. Багийн нэр
-//                 </label>
-//                 <input
-//                   value={form.FirstName}
-//                   onChange={(e) => {
-//                     setForm((prevState) => ({
-//                       ...prevState,
-//                       FirstName: e.target.value,
-//                     }));
-//                     setFormErrorState((prevState) => ({
-//                       ...prevState,
-//                       FirstName: false,
-//                     }));
-//                   }}
-//                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                   placeholder="Нэр"
-//                 />
-//                 {formErrorState.FirstName && (
-//                   <p className="text-red-600 text-sm">Нэрээ бичнэ үү</p>
-//                 )}
-//               </div>
-//               <div className="grid md:grid-cols-2 gap-6 mb-6">
-//                 <div>
-//                   <label className="font-bold block mb-2">3. Овог</label>
-//                   <input
-//                     value={form.LastName}
-//                     onChange={(e) => {
-//                       setForm((prevState) => ({
-//                         ...prevState,
-//                         LastName: e.target.value,
-//                       }));
-//                       setFormErrorState((prevState) => ({
-//                         ...prevState,
-//                         LastName: false,
-//                       }));
-//                     }}
-//                     className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                     placeholder="Овог"
-//                   />
-//                   {formErrorState.LastName && (
-//                     <p className="text-red-600 text-sm">Овгоо бичнэ үү.</p>
-//                   )}
-//                 </div>
-//                 <div>
-//                   <label className="font-bold block mb-2">4. Нэр</label>
-//                   <input
-//                     value={form.FirstName}
-//                     onChange={(e) => {
-//                       setForm((prevState) => ({
-//                         ...prevState,
-//                         FirstName: e.target.value,
-//                       }));
-//                       setFormErrorState((prevState) => ({
-//                         ...prevState,
-//                         FirstName: false,
-//                       }));
-//                     }}
-//                     className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                     placeholder="Нэр"
-//                   />
-//                   {formErrorState.FirstName && (
-//                     <p className="text-red-600 text-sm">Нэрээ бичнэ үү</p>
-//                   )}
-//                 </div>
-//               </div>
-//               <div className="grid md:grid-cols-2 gap-6 mb-6">
-//                 <div>
-//                   <label className="font-bold block mb-2">5. Нас</label>
-//                   <select
-//                     value={form.age}
-//                     onChange={(e) => {
-//                       setForm((prevState) => ({
-//                         ...prevState,
-//                         age: e.target.value,
-//                       }));
-//                       setFormErrorState((prevState) => ({
-//                         ...prevState,
-//                         age: false,
-//                       }));
-//                     }}
-//                     className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                   >
-//                     <option value="">Нас сонгох</option>
-//                     {[...Array(9)].map((_, i) => (
-//                       <option key={i} value={(i + 6).toString()}>
-//                         {i + 6}
-//                       </option>
-//                     ))}
-//                   </select>
-//                   {formErrorState.age && (
-//                     <p className="text-red-600 text-sm">Насаа оруулна уу</p>
-//                   )}
-//                 </div>
-//                 <div>
-//                   <label className="font-bold block mb-2 md:pl-3">
-//                     6. Хүйс
-//                   </label>
-//                   <div className="flex gap-4 pl-3 items-center">
-//                     <label>
-//                       <input
-//                         type="radio"
-//                         value="male"
-//                         className="w-4 h-4"
-//                         onChange={(e) => {
-//                           setForm((prevState) => ({
-//                             ...prevState,
-//                             gender: e.target.value,
-//                           }));
-//                           setFormErrorState((prevState) => ({
-//                             ...prevState,
-//                             gender: false,
-//                           }));
-//                         }}
-//                       />{" "}
-//                       Эрэгтэй
-//                     </label>
-//                     <label>
-//                       <input
-//                         type="radio"
-//                         onChange={(e) => {
-//                           setForm((prevState) => ({
-//                             ...prevState,
-//                             gender: e.target.value,
-//                           }));
-//                           setFormErrorState((prevState) => ({
-//                             ...prevState,
-//                             gender: false,
-//                           }));
-//                         }}
-//                         value="female"
-//                         className="w-4 h-4"
-//                       />{" "}
-//                       Эмэгтэй
-//                     </label>
-//                   </div>
-//                   {formErrorState.gender && (
-//                     <p className="text-red-600 text-sm">Хүйсээ сонгоно уу</p>
-//                   )}
-//                 </div>
-//               </div>
-//               <div className="mb-6">
-//                 <label className="font-bold block mb-2">
-//                   8. Холбоо барих утас
-//                 </label>
-//                 <input
-//                   type="number"
-//                   value={form.contactPhone}
-//                   onChange={(e) => {
-//                     setForm((prevState) => ({
-//                       ...prevState,
-//                       contactPhone: e.target.value,
-//                     }));
-//                     setFormErrorState((prevState) => ({
-//                       ...prevState,
-//                       contactPhone: false,
-//                     }));
-//                   }}
-//                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
-//                   placeholder="Утасны дугаар"
-//                 />
-//                 {formErrorState.contactPhone && (
-//                   <p className="text-red-600 text-sm">
-//                     Утасны дугаар оруулна уу
-//                   </p>
-//                 )}
-//               </div>
-
-//               <button
-//                 type="button"
-//                 onClick={handleCreateInvoice}
-//                 className="w-full p-2 rounded bg-gradient-to-r from-blue-500 to-emerald-600 text-white hover:bg-blue-600"
-//               >
-//                 Төлбөр төлөх
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -408,7 +30,7 @@ export default function RegistrationForm() {
     teamName: false,
     firstNames: [] as boolean[],
     lastNames: [] as boolean[],
-    teachername: false,
+    teacherName: false,
     ages: false,
     employmentStatus: false,
     contactPhone: false,
@@ -448,7 +70,7 @@ export default function RegistrationForm() {
     if (form.ages === "") errors.ages = true;
     if (form.contactPhone === "" || form.contactPhone.length !== 8)
       errors.contactPhone = true;
-    if (form.teacherName === "") errors.teachername = true;
+    if (form.teacherName === "") errors.teacherName = true;
 
     const selectedClass = classData.find((c) => c._id === form.trainingType);
     const studentNumber = selectedClass?.studentNumber || 0;
@@ -517,14 +139,14 @@ export default function RegistrationForm() {
                     setForm((prev) => ({
                       ...prev,
                       trainingType: e.target.value,
-                      FirstName: [],
-                      LastName: [],
+                      firstNames: [],
+                      lastNames: [],
                     }));
                     setFormErrorState((prev) => ({
                       ...prev,
                       trainingType: false,
-                      FirstName: [],
-                      LastName: [],
+                      firstNames: [],
+                      lastNames: [],
                     }));
                   }}
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
@@ -578,7 +200,7 @@ export default function RegistrationForm() {
                     }));
                     setFormErrorState((prevState) => ({
                       ...prevState,
-                      SchoolName: false,
+                      schoolName: false,
                     }));
                   }}
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
@@ -671,13 +293,13 @@ export default function RegistrationForm() {
                     }));
                     setFormErrorState((prevState) => ({
                       ...prevState,
-                      teachername: false,
+                      teacherName: false,
                     }));
                   }}
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
                   placeholder="Багшийн нэр"
                 />
-                {formErrorState.teachername && (
+                {formErrorState.teacherName && (
                   <p className="text-red-600 text-sm">Багшийн нэрээ бичнэ үү</p>
                 )}
               </div>
@@ -713,18 +335,25 @@ export default function RegistrationForm() {
                 <input
                   type="number"
                   value={form.contactPhone}
-                  onChange={(e) =>
+                  onChange={(e) => {
                     setForm((prev) => ({
                       ...prev,
                       contactPhone: e.target.value,
-                    }))
-                  }
+                    }));
+                    if (e.target.value.length === 8) {
+                      setFormErrorState((prev) => ({
+                        ...prev,
+                        contactPhone: false,
+                      }));
+                    }
+                  }}
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-green-500"
                   placeholder="Утасны дугаар"
                 />
+
                 {formErrorState.contactPhone && (
                   <p className="text-red-600 text-sm">
-                    Утасны дугаар оруулна уу
+                    Утасны дугаар гүйцэт оруулна уу
                   </p>
                 )}
               </div>
